b9eec096a2a3b981277eb03ea311ff590a8350d8
Marcos52000 <55589715+Marcos52000@users.noreply.github.com>
2022-03-08 11:31:25 +0100

    commit

---
+++ UD32/ej3/js/act3.js
@@ -0,0 +1,10 @@
+window.onload = function (){
+    var meses = ["Enero", "Febero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+
+    for (var i = 0; i < meses.length; i++) {
+        console.log(meses[i]);
+    }
+
+}
+
+
